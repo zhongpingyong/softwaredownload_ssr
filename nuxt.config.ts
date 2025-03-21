a4c12afaceea282357fe7a6626bb35df9bd213ad
zhongpingyong <zhongpingyong88@163.com>
2025-03-21 15:37:48 +0800

    新增GTM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,13 @@ if (process.env.NODE_ENV === 'production') {
     async: true
   })
 }
+scripts.push({
+  innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5XZDXM54');`
+})
 export default defineNuxtConfig({
   telemetry: false,
   ssr: false,
@@ -47,7 +54,12 @@ export default defineNuxtConfig({
           content: 'Download LaserPecker PC software for Windows & Mac here. This software is compatible with all LaserPecker machines like LaserPecker 2, 3, LP4, LX1, and LX1 Max.'
         }
       ],
-      script: scripts
+      script: scripts,
+      noscript: [{
+        innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XZDXM54"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        tagPosition: 'bodyOpen',
+      }]
     }
   },
   // nitro: {
